351bc9dd4108740d8ebb92da2e0e3192f4d195cd
Weinan Li <weli@redhat.com>
2014-08-01 20:52:06 +0800

    initial codes

---
+++ src/main/java/org/arq/example/RegistryApplication.java
@@ -0,0 +1,9 @@
+package org.arq.example;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("registry")
+public class RegistryApplication extends Application {
+
+}
